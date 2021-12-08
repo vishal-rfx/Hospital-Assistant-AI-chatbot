c30422f412a7496f93d8aa0784e58f02b029a1f1
vishal-rfx <vishal23rfx@gmail.com>
2021-12-08 23:44:59 +0530

    added lambda validation and fulfillment

--- amplify/backend/function/getCovidTestResultIntent/src/index.js
+++ amplify/backend/function/getCovidTestResultIntent/src/index.js
@@ -7,16 +7,193 @@
 	STORAGE_COVIDTESTDETAILSDB_STREAMARN
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+
+const COVID_NEG = (intentReq,name) => {
+    return {
+        sessionAttributes : intentReq.sessionAttributes,
+        dialogAction : {
+            type :"Close",
+            fulfillmentState : "Fulfilled",
+            message: {
+                contentType : "PlainText",
+                content: `Dear ${name} We are very happy to say that your covid result is comes out to be negative. Please wear mask all the time for precaution`
+            }
+        }
+    }
+}
+
+const COVID_POS = (intentReq,name) => {
+    return {
+        sessionAttributes : intentReq.sessionAttributes,
+        dialogAction : {
+            type :"Close",
+            fulfillmentState : "Fulfilled",
+            message: {
+                contentType : "PlainText",
+                content: `Dear ${name} We are sorry to say that you are covid positive. Please visit a nearby hospital. We wish you a speedy recovery`
+            }
+        }
+    }
+}
+
+const COVID_PROCESS = (intentReq,name) => {
+    return {
+        sessionAttributes : intentReq.sessionAttributes,
+        dialogAction : {
+            type :"Close",
+            fulfillmentState : "Fulfilled",
+            message: {
+                contentType : "PlainText",
+                content: `Dear ${name} your covid test result is still not out yet. Please be patient and don't get panicked.`
+            }
+        }
+    }
+}
+
+const WRONG = (intentReq,name) => {
+    return {
+        sessionAttributes : intentReq.sessionAttributes,
+        dialogAction : {
+            type :"Close",
+            fulfillmentState : "Fulfilled",
+            message: {
+                contentType : "PlainText",
+                content: `Dear ${name} My dev is lazy goose pls wait till he fix me`
+            }
+        }
+    }
+}
+
+const response_format = {
+  sessionAttributes: {},
+  dialogAction: {
+    type: "Close",
+    fulfillmentState: "Fulfilled",
+    message: {
+      contentType: "PlainText",
+      content: "Its fulfilled",
+    },
+  },
+};
+
+const delegate = (sessionAttributes, slots) => {
+  return {
+    sessionAttributes,
+    dialogAction: {
+      type: "Delegate",
+      slots,
+    },
+  };
+};
+
+const elicitSlot = (sessionAttrs, intentName, slots, slotToElicit, message) => {
+  return {
+    sessionAttrs: sessionAttrs,
+    dialogAction: {
+      type: "ElicitSlot",
+      intentName: intentName,
+      slots: slots,
+      slotToElicit: slotToElicit,
+      message: message,
+    },
+  };
+};
+
+const fulfill = async (intentReq) => {
+    const userRef = intentReq.currentIntent.slots.UserRefID.toLowerCase()
+
+    const params = {
+        TableName : process.env.STORAGE_COVIDTESTDETAILSDB_NAME,
+        Key : {
+            user_ref_id : userRef
+        },
+    }
+
+    const data = await docClient.get(params).promise()
+    let name = data.Item.name
+    let status = data.Item.status
+
+    if(status == "0"){
+        return COVID_PROCESS(intentReq,name)
+    }
+
+    if(status == "1"){
+        return COVID_POS(intentReq,name)
+    }
+
+    if(status == "-1"){
+        return COVID_NEG(intentReq,name)
+    }
+    
+
+};
+
+const validateUserRefId = async (userRefId) => {
+  let id = userRefId.toLowerCase();
+  const params = {
+    TableName: process.env.STORAGE_COVIDTESTDETAILSDB_NAME,
+    Key: {
+      user_ref_id: id,
+    },
+  };
+
+  const data = await docClient.get(params).promise();
+
+  if (Object.keys(data.Item).length === 0) {
+    return {
+      isValid: false,
+      violatedSlot: "UserRefID",
+      message: {
+        contentType: "PlainText",
+        content:
+          "Patient Reference ID should not contain alphabets, Please provide a valid Patient Reference ID",
+      },
     };
-    return response;
+  }
+
+  return {
+    isValid: true,
+    violatedSlot: "No Violation",
+  };
+};
+
+const getTestResultIntent = (intentReq) => {
+  const source = intentReq.invocationSource;
+  if (source === "DialogueCodeHook") {
+    const slots = intentReq.currentIntent.slots;
+    const userRefId = intentReq.currentIntent.slots.UserRefID;
+    const validateResult = validateUserRefId(userRefId);
+
+    if (!validateResult.isValid) {
+      slots.UserRefID = null;
+      return elicitSlot(
+        intentReq.sessionAttributes,
+        intentReq.currentIntent.name,
+        slots,
+        validateResult.violatedSlot,
+        validateResult.message
+      );
+    }
+
+    return delegate(intentReq.sessionAttributes, intentReq.currentIntent.slots);
+  } else if (source === "FulfillmentCodeHook") {
+    return fulfill(intentReq);
+  }
+};
+
+const dispatch = (intentReq) => {
+  const userRefId = intentReq.currentIntent.slots.UserRefID;
+  if (userRefId === null) {
+    return delegate(intentReq.sessionAttributes, intentReq.currentIntent.slots);
+  }
+
+  return getTestResultIntent(intentReq);
+};
+
+exports.handler = async (event) => {
+  const response = dispatch(event);
+  return response;
 };
